1b7b4b5e1c5e7c1adf0c52b0c070becacb9360b7
claudiocbams <cpcclaudio.pereira@gmail.com>
2020-03-30 19:47:03 -0300

    cliente tipocliente telefones e endereços

--- src/main/java/com/claudio/cursomc/CursomcApplication.java
+++ src/main/java/com/claudio/cursomc/CursomcApplication.java
@@ -9,10 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.claudio.cursomc.domain.Categoria;
 import com.claudio.cursomc.domain.Cidade;
+import com.claudio.cursomc.domain.Cliente;
+import com.claudio.cursomc.domain.Endereco;
 import com.claudio.cursomc.domain.Estado;
 import com.claudio.cursomc.domain.Produto;
+import com.claudio.cursomc.domain.enums.TipoCliente;
 import com.claudio.cursomc.repositories.CategoriaRepository;
 import com.claudio.cursomc.repositories.CidadeRepository;
+import com.claudio.cursomc.repositories.ClienteRepository;
+import com.claudio.cursomc.repositories.EnderecoRepository;
 import com.claudio.cursomc.repositories.EstadoRepository;
 import com.claudio.cursomc.repositories.ProdutoRepository;
 
@@ -28,6 +33,12 @@ public class CursomcApplication implements CommandLineRunner {
      
    @Autowired
   	private EstadoRepository estadoRepository;
+   
+   @Autowired
+ 	private ClienteRepository clienteRepository;
+  @Autowired
+	private EnderecoRepository enderecoRepository;
+   
      
    
 	public static void main(String[] args) {
@@ -75,13 +86,35 @@ public class CursomcApplication implements CommandLineRunner {
 		est5.getCidades().addAll(Arrays.asList(c4));
 		est6.getCidades().addAll(Arrays.asList(c5,c6));
 		
+		Cliente cli1 = new Cliente(null, "Maria socorro","maria@gmail.com", "544.592.984-11", TipoCliente.PESSOAFISICA);
+		cli1.getTelefones().addAll(Arrays.asList("6732270683", "6999845522"));
+		Cliente cli2 = new Cliente(null, "carlos","carlos@gmail.com", "644.592.982-09", TipoCliente.PESSOAFISICA);
+		cli2.getTelefones().addAll(Arrays.asList("6632255583", "6999445523"));
+		Cliente cli3 = new Cliente(null, "Marcola","marcolino@gmail.com", "474.592.984-15", TipoCliente.PESSOAFISICA);
+		cli3.getTelefones().addAll(Arrays.asList("6833212383", "6999865526"));
+		Cliente cli4 = new Cliente(null, "Marcolino","marcola@gmail.com", "354.592.984-10", TipoCliente.PESSOAJURIDICA);
+		cli4.getTelefones().addAll(Arrays.asList("6932270683", "6999845545"));
+		Cliente cli5 = new Cliente(null, "Marcelo","marcelino@gmail.com", "244.592.984-16", TipoCliente.PESSOAJURIDICA);
+		cli5.getTelefones().addAll(Arrays.asList("6932377683", "6998845596"));
+		
+		Endereco end1 = new Endereco(null, "Rua São Paulo","453", "Condominio Aquarius", "Planalto", "76845-011",cli1, c2);
+		Endereco end2 = new Endereco(null, "Rua São joão","7773", "Condominio Azaleia", "Madeireira", "79290-071",cli1, c1);
+		Endereco end3 = new Endereco(null, "Av São José","77", "Condominio Amarilis", "Rio Madeira", "76875-021",cli3, c3);
+		Endereco end4 = new Endereco(null, "Rua São sebastião","9978", "Condominio Rio Madeira", "Lixeira", "77845-251",cli4, c4);
+		Endereco end5 = new Endereco(null, "Rua Rio de janeiro","73", "Condominio Rio Santos", "Paigauas", "78535-031",cli5, c6);
+		
+		cli1.getEnderecos().addAll(Arrays.asList(end1, end2));
+		cli3.getEnderecos().addAll(Arrays.asList(end3));
+		cli3.getEnderecos().addAll(Arrays.asList(end4));
+		cli3.getEnderecos().addAll(Arrays.asList(end5));
 		
 		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5,p6,p7));
 		estadoRepository.saveAll(Arrays.asList(est1,est2, est3, est4, est4, est5, est6));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3,c4,c5,c6,c7));
-		
+		clienteRepository.saveAll(Arrays.asList(cli1,cli2,cli3,cli4,cli5));
+		enderecoRepository.saveAll(Arrays.asList(end1,end2,end3,end4,end5));
 		
 	}
 
---
+++ src/main/java/com/claudio/cursomc/domain/enums/TipoCliente.java
@@ -0,0 +1,49 @@
+package com.claudio.cursomc.domain.enums;
+
+public enum TipoCliente {
+	PESSOAFISICA(1, "Pessoa Fisíca"),
+	PESSOAJURIDICA(2, "Pessoa Juridica");
+	
+	private int cod;
+	private String descricao;
+	
+	private TipoCliente(int cod, String descricao) { //enumeração deve ser tipo private 
+		this.cod = cod;
+		this.descricao = descricao;
+		
+	}
+
+	public int getCod() {
+		return cod;
+	}
+
+	
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	
+	public static TipoCliente toEnum(Integer cod)
+	{
+		if(cod == null)
+		{
+			return null;
+		}
+		
+		for (TipoCliente x : TipoCliente.values())
+		{
+			if(cod.equals(x.getCod()))
+			{
+				return x;
+			}
+		}
+		throw new IllegalArgumentException ("Id Inválido" + cod);
+	}
+	
+	
+}
+	
+	
+
+
