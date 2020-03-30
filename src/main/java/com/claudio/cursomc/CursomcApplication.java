e3204ecb77e48a60be792060b7d6498c4c4595f0
claudiocbams <cpcclaudio.pereira@gmail.com>
2020-03-30 12:37:42 -0300

    Classe produto e associação muitos para muitos

--- src/main/java/com/claudio/cursomc/CursomcApplication.java
+++ src/main/java/com/claudio/cursomc/CursomcApplication.java
@@ -8,12 +8,18 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.claudio.cursomc.domain.Categoria;
+import com.claudio.cursomc.domain.Produto;
 import com.claudio.cursomc.repositories.CategoriaRepository;
+import com.claudio.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner {
    @Autowired
 	private CategoriaRepository categoriaRepository;
+   
+   @Autowired
+	private ProdutoRepository produtoRepository;
+   
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -21,7 +27,27 @@ public class CursomcApplication implements CommandLineRunner {
 	public void run(String ...args) throws Exception{
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		
+		Produto p1 = new Produto(null, "Computador", 2000.00);
+		Produto p2 = new Produto(null, "Impressora", 1000.00);
+		Produto p3 = new Produto(null, "Mouse", 20.00);
+		Produto p4 = new Produto(null, "Teclado", 40.00);
+		Produto p5 = new Produto(null, "Caderno", 35.00);
+		Produto p6 = new Produto(null, "Cadeira", 180.00);
+		Produto p7 = new Produto(null, "Mesa", 400.00);
+		cat1.getProdutos().addAll(Arrays.asList(p1, p2,p3,p4));
+		cat2.getProdutos().addAll(Arrays.asList(p5, p6,p7));
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
+		p4.getCategorias().addAll(Arrays.asList(cat1));
+		p5.getCategorias().addAll(Arrays.asList(cat2));
+		p6.getCategorias().addAll(Arrays.asList(cat2));
+		p7.getCategorias().addAll(Arrays.asList(cat2));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5,p6,p7));
+		
 		
 	}
 
--- src/main/java/com/claudio/cursomc/domain/Categoria.java
+++ src/main/java/com/claudio/cursomc/domain/Categoria.java
@@ -1,11 +1,14 @@
 package com.claudio.cursomc.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 @Entity
 public class Categoria implements Serializable {
 	
@@ -17,7 +20,17 @@ public class Categoria implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private String nome;
-    public Categoria () {
+	
+	@ManyToMany(mappedBy = "categorias")
+	private List<Produto> produtos = new ArrayList<Produto>();
+	
+    public List<Produto> getProdutos() {
+		return produtos;
+	}
+	public void setProdutos(List<Produto> produtos) {
+		this.produtos = produtos;
+	}
+	public Categoria () {
     	
     }
 	public Categoria(Integer id, String nome) {
