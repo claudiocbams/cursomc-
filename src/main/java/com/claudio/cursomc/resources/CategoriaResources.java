365aac13e9eadbe82026fbfeeddd47369ee8493d
claudiocbams <cpcclaudio.pereira@gmail.com>
2020-03-31 22:45:07 -0300

    Atualizando categoria com o push e outros ajustes

--- src/main/java/com/claudio/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/claudio/cursomc/resources/CategoriaResources.java
@@ -3,12 +3,16 @@ package com.claudio.cursomc.resources;
 
 
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.claudio.cursomc.domain.Categoria;
 import com.claudio.cursomc.services.CategoriaService;
@@ -23,6 +27,13 @@ public class CategoriaResources {
 		Categoria obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 		
+	}
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> Insert(@RequestBody  Categoria obj)
+	{
+		obj = service.Insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 
 }
