0068eba2fc2660df34e12c40ac706643bdf5e46b
claudiocbams <cpcclaudio.pereira@gmail.com>
2020-04-02 00:20:38 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/claudio/cursomc/resources/CategoriaResources.java
+++ src/main/java/com/claudio/cursomc/resources/CategoriaResources.java
@@ -8,11 +8,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -51,6 +53,18 @@ public class CategoriaResources {
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> lista = service.findAll();
 		List<CategoriaDTO> listaDTO = lista.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());// service.findAll();
+		return ResponseEntity.ok().body(listaDTO);
+	}
+	
+
+	@RequestMapping(value= "/page", method=RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage( 
+		@RequestParam(value ="page", defaultValue = "0")Integer pages,
+		@RequestParam(value ="linesPerPage", defaultValue = "24")Integer linesPerPage,
+		@RequestParam(value ="orderBy", defaultValue = "nome")String orderBy,
+		@RequestParam(value ="direction", defaultValue = "ASC") String direction){
+		Page<Categoria> lista = service.findPage( pages,  linesPerPage, orderBy, direction);
+		Page<CategoriaDTO> listaDTO = lista.map(obj -> new CategoriaDTO(obj));// service.findAll();
 		return ResponseEntity.ok().body(listaDTO);
 	}
 
--- src/main/java/com/claudio/cursomc/services/CategoriaService.java
+++ src/main/java/com/claudio/cursomc/services/CategoriaService.java
@@ -5,6 +5,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.claudio.cursomc.domain.Categoria;
@@ -49,5 +52,9 @@ public class CategoriaService {
 		
 		return repo.findAll();
 	}
-
+	
+	public Page<Categoria> findPage(Integer pages, Integer linesPerPage, String orderBy, String direction ){
+		PageRequest pageRequest = PageRequest.of(pages, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+	}
 }
